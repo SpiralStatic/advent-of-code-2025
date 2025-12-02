f213299ef687016e5ca9913273894be66a75773c
Hannah Tsoi <hannah.tsoi+work@protonmail.com>
2025-12-02 03:41:29 +0000

    day 1 - calculate dial left right distance total

--- day1-secret-entrance/SecretEntrance/SafeBreaker.cs
+++ day1-secret-entrance/SecretEntrance/SafeBreaker.cs
@@ -1,6 +1,8 @@
+using System.Text.RegularExpressions;
+
 namespace SecretEntrance;
 
-public class SafeBreaker
+public partial class SafeBreaker
 {
   public static async Task<IEnumerable<string>> ReadRotationsInput(string path)
   {
@@ -9,6 +11,45 @@ public class SafeBreaker
 
   public static int SolveSafePassword(List<string> rotations)
   {
-    return 0;
+    var (lTotalDistanc, rTotalDistance) = rotations.Aggregate((0, 0), (total, next) =>
+    {
+      var directionDistance = RotationsRegex().Split(next);
+
+      var (direction, distance) = CreateDirectionDistanceTuple(directionDistance);
+
+      var (lTotal, rTotal) = total;
+
+      if (direction == 'L')
+      {
+        return (lTotal + distance, rTotal);
+      }
+      else
+      {
+        return (lTotal, rTotal + distance);
+      }
+    });
+
+    var dial = 50 - lTotalDistanc + rTotalDistance;
+
+    return dial;
   }
+
+  private static (char, int) CreateDirectionDistanceTuple(string[] directionDistance)
+  {
+    if (directionDistance.Length > 2)
+    {
+      throw new Exception($"{nameof(directionDistance)} has too many components");
+    }
+
+    var directionSuccess = char.TryParse(directionDistance[0], out char direction);
+    var distanceSuccess = int.TryParse(directionDistance[1], out int distance);
+
+    if (!directionSuccess) throw new FormatException("Invalid Direction, not a char");
+    if (!distanceSuccess) throw new FormatException("Invalid Distance, not an int");
+
+    return (direction, distance);
+  }
+
+  [GeneratedRegex(@"(?<=[LR])")]
+  private static partial Regex RotationsRegex();
 }
